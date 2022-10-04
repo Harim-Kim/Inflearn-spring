13cf80dfb15579b3da4d9c8f813b041c512a6633
yongkal2@yonsei.ac.kr <yongkal2@yonsei.ac.kr>
2022-10-04 21:07:48 +0900

    spring fundamental

---
+++ src/main/java/hello/core/order/OrderServceImpl.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // interface에만 의존 함 --> NPE 에러 발생.
+    //final로 맴버변수 생성시 생성자에서 모두 주입 받아야 함.
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private  final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    public OrderServceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
